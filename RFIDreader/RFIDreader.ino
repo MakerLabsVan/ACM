27e333a53ad75983c8a0e94288a977b600c6bf73
brian-nguyen <briannguyen992@gmail.com>
2016-06-01 11:54:00 -0700

    accumulator function implemented

--- RFIDreader/RFIDreader.ino
+++ RFIDreader/RFIDreader.ino
@@ -26,10 +26,10 @@ char txrxbuffer[255];
 // response packet when idle - 0xAA, 0x00, 0x02, 0x01, 0x83, 0x80, 0xBB
 char get_readID[] = { 0xAA , 0x00, 0x03, 0x25, 0x26, 0x00, 0x00, 0xBB };
 byte response[11];
-bool standby;
-unsigned long elapsed_time;
 const int ledPin = 13;
 const int signalPin = 4;
+bool standby;
+unsigned long elapsedTime;
 
 void setup() {
   Serial.begin(57600);
@@ -42,10 +42,9 @@ void loop() {
   // send command packet to RDM880
   RDM880.write(get_readID, 8);
   delay(100);
-  digitalWrite(ledPin, LOW); 
-  standby = true;
+  digitalWrite(ledPin, LOW);
   response[7] = NULL;
-  elapsed_time = 0;
+  standby = true;
 
   // read response from RDM880 and print
   int i = 0;
@@ -54,18 +53,14 @@ void loop() {
     Serial.print(response[i], HEX);
     Serial.print(" ");
     i++;
-
-    // LED on if an RFID is detected (idle packet only contains 7 bytes)
-    if(response[7] != NULL) {
-      digitalWrite(ledPin, HIGH);
-      // Machine is ready to fire, get ready to track time
-      standby = false;
-      elapsed_time = accumulator(standby) / 1000;
-      if (elapsed_time != 0) {
-        Serial.print("Elapsed Time: ");
-        Serial.print(elapsed_time);
-      }
-    }
+  }
+  
+  // LED on if an RFID is detected (idle packet only contains 7 bytes)
+  if(response[7] != NULL) {
+    digitalWrite(ledPin, HIGH);
+    // Machine is ready to fire, get ready to track time
+    standby = false;
+    elapsedTime = accumulator(standby);
   }
   Serial.println();
 }
@@ -75,7 +70,7 @@ unsigned long accumulator(bool standby) {
   int signalState;
   int previousState = NULL;
   
-  while(!standby) {
+  while(standby == false) {
     // read signal state
     signalState = digitalRead(signalPin);
 
@@ -86,16 +81,19 @@ unsigned long accumulator(bool standby) {
     }
     // check for OFF signal
     else if (previousState == HIGH && signalState == LOW) {
-      endTime = millis();
-      // calculate elapsed time
-      return endTime - startTime;
+      endTime = (millis() - startTime)/1000;
+      if (endTime != 0) {
+        Serial.print("Elapsed time: ");
+        Serial.println(endTime);
+      }
+      previousState = signalState;
+      standby = true;
     }
     // no event
     else {
       previousState = signalState;
     }
   }
-  return 0;
 }
 
 
