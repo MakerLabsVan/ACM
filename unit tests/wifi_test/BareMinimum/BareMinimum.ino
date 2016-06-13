868701bf31def4bae96bde36d693f99fd638a81b
Brian Nguyen <briannguyen992@gmail.com>
2016-06-13 16:54:06 -0700

    AT commands for HTTP GET

---
+++ unit tests/wifi_test/BareMinimum/BareMinimum.ino
@@ -0,0 +1,14 @@
+/*
+	AT+RST
+	AT+CIPMUX=0
+	AT+CIPSTART=0,"TCP","www.makerlabs.com",80
+	AT+CIPSEND=0,46
+	GET / HTTP/1.1\r\n Host: www.makerlabs.com\r\n
+*/
+
+void setup() {
+}
+
+void loop() {
+  
+}
