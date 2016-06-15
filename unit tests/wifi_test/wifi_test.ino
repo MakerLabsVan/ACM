0ef19e2ed93fa32c3746cffbeefe940749380873
Brian Nguyen <briannguyen992@gmail.com>
2016-06-14 19:08:14 -0700

    Communication between Arduino and Wi-Fi modulesuccessfully established

--- unit tests/wifi_test/wifi_test.ino
+++ unit tests/wifi_test/wifi_test.ino
@@ -1,16 +1,19 @@
 #include <SoftwareSerial.h>
- 
-#define DEBUG true
- 
-SoftwareSerial esp8266(2,3); // make RX Arduino line is pin 2, make TX Arduino line is pin 3.
-                             // This means that you need to connect the TX line from the esp to the Arduino's pin 2
-                             // and the RX line from the esp to the Arduino's pin 3
+
+SoftwareSerial ESP(2,3);
+
 void setup()
 {
-  
+    Serial.begin(9600);
+    ESP.begin(9600);
+    Serial.println("such wow");
 }
- 
 void loop()
 {
-  
+    while (ESP.available()) {
+        Serial.write(ESP.read());
+    }
+    while (Serial.available()) {
+        ESP.write(Serial.read());
+    }
 }
