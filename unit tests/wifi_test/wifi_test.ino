6e4f37b0759cd97de36826135bf68a5baaa93a79
Brian Nguyen <briannguyen992@gmail.com>
2016-06-16 12:19:00 -0700

    testing wifi connection

--- unit tests/wifi_test/wifi_test.ino
+++ unit tests/wifi_test/wifi_test.ino
@@ -6,17 +6,29 @@ void setup()
 {
     Serial.begin(9600);
     ESP.begin(9600);
+    connectWiFi();
 }
 void loop()
 {
     unsigned long startTime = millis();
 
-    while (ESP.available()) {
+    /*while (ESP.available()) {
         Serial.write(ESP.read());
     }
-    while ( (millis() - startTime) < 500 ) {
-    }
+    while (Serial.available()) {
+        ESP.write(Serial.read());
+    }*/
+}
 
-    ESP.write("AT\r\n");
-    
+void connectWiFi(void) {
+    ESP.write("AT+CWMODE=1\r\n");
+    delay(2000);
+    ESP.write("AT+CWJAP=\"MakerLabs\",\"ecordova\"\r\n");
+    delay(5000);
+    if(ESP.find("OK")) {
+        Serial.write("Connected\n");
+    }
+    else {
+        Serial.write("Failed to connect\n");
+    }
 }
