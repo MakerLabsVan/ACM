460c7a9a8daea889852f4474801679f30ab23664
Brian Nguyen <briannguyen992@gmail.com>
2016-06-20 11:52:43 -0700

    forgot to save wifi changes

--- module/module_wifi.ino
+++ module/module_wifi.ino
@@ -2,40 +2,40 @@
     All Wi-Fi related functions
 */
 void connectWIFI(void) {
-    ESP.write("AT+CWQAP\r\n");
+    WIFI.write("AT+CWQAP\r\n");
     delay(50);
-    ESP.write("AT+CWMODE=1\r\n");
+    WIFI.write("AT+CWMODE=1\r\n");
     delay(50);
-    ESP.write("AT+CWJAP=\"MakerLabs\",\"ecordova\"\r\n");
-    while(!ESP.find("WIFI GOT IP"));
+    WIFI.write("AT+CWJAP=\"MakerLabs\",\"ecordova\"\r\n");
+    while(!WIFI.find("WIFI GOT IP"));
     Serial.write("Connected. ");
-    while(!ESP.find("OK"));
+    while(!WIFI.find("OK"));
     Serial.write("Ready.\n\n");
 }
 
 void startConnection(void) {
     Serial.write("Connecting...\n");
-    ESP.write("AT+CIPMUX=1\r\n");
+    WIFI.write("AT+CIPMUX=1\r\n");
     delay(50);
-    ESP.write("AT+CIPSTART=0,\"TCP\",\"www.httpbin.org\",80\r\n");
-    while(!ESP.find("CONNECT"));
-    while(!ESP.find("OK"));
+    WIFI.write("AT+CIPSTART=0,\"TCP\",\"www.httpbin.org\",80\r\n");
+    while(!WIFI.find("CONNECT"));
+    while(!WIFI.find("OK"));
     Serial.write("Connection established.\n\n");
     
 }
 
 void closeConnection(void) {
     Serial.write("Closing connection...\n");
-    ESP.write("AT+CIPCLOSE=0\r\n");
-    while(!ESP.find("CLOSED"));
-    while(!ESP.find("OK"));
+    WIFI.write("AT+CIPCLOSE=0\r\n");
+    while(!WIFI.find("CLOSED"));
+    while(!WIFI.find("OK"));
     Serial.write("Connection closed.\n\n");
 }
 
 void GET(void) {
-    ESP.write("AT+CIPSEND=0,43\r\n");
-    while(!ESP.find("ERROR"));
-    ESP.write("AT+CIPSEND=0,43\r\n");
+    WIFI.write("AT+CIPSEND=0,43\r\n");
+    while(!WIFI.find("ERROR"));
+    WIFI.write("AT+CIPSEND=0,43\r\n");
     delay(50);
-    ESP.write("GET /ip HTTP/1.1\r\nHost: www.httpbin.org\r\n\r\n");
+    WIFI.write("GET /ip HTTP/1.1\r\nHost: www.httpbin.org\r\n\r\n");
 }
