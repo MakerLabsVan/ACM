ccb8cd29d26774e93c1eb4fc939c800950edf53b
Brian Nguyen <briannguyen992@gmail.com>
2016-06-23 17:19:43 -0700

    tested on laser cutter

--- unit tests/pwm_detector_test/pwm_detector_test.ino
+++ unit tests/pwm_detector_test/pwm_detector_test.ino
@@ -1,18 +1,46 @@
 const int pwmPin = 5;
 const int isrPin = 2;
+const int halfduty = 127;
+const unsigned long frequency = 14550;
+const unsigned long pollInterval = 3000;
 volatile unsigned long pulseCount = 0;
 
 void setup() {
 	Serial.begin(57600);
 	Serial.print("Initializing...");
 	pinMode(pwmPin, OUTPUT);
-	pinmode(isrPin, INPUT);
+	pinMode(isrPin, INPUT);
 	attachInterrupt(digitalPinToInterrupt(isrPin), pwmDetect, FALLING);
+	analogWrite(pwmPin, halfduty);
 }
 
 void loop() {
-	Serial.println(pulseCount);
+	unsigned long startTime = 0;
+	volatile unsigned long currentCount = 0;
+
+	if (pulseCount == 1) {
+		startTime = millis();
+		Serial.println("First pulse detected");
+	}
+	else if (pulseCount > 1) {
+		currentCount = pulseCount;
+		Serial.print(currentCount);
+		Serial.print(" ");
+		Serial.println(currentCount/frequency);
+		/*if ((millis() - startTime) > pollInterval) {
+			if (pulseCount == currentCount) {
+				Serial.print(pulseCount/frequency);
+				pulseCount = 0;
+			}
+		}*/
+	}
+	else {
+		pulseCount = 0;
+	}
 }
+
+
+
 
 void pwmDetect() {
 	pulseCount += 1;
