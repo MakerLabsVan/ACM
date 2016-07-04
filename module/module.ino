e9b63a7e6c746581ee2988faf5a9d0af473dec1e
Brian Nguyen <briannguyen992@gmail.com>
2016-07-04 14:38:18 -0700

    Testing..

--- module/module.ino
+++ module/module.ino
@@ -149,7 +149,7 @@ unsigned long getTime (unsigned char readData[]) {
 */
 unsigned long accumulator(void) {
 	unsigned char A[bufferSize];
-	unsigned long startTime, endTime = 0;
+	unsigned long startTime = 0;
 	unsigned int periodX, periodY;
 	unsigned int lastPeriodX, lastPeriodY; 
 	unsigned long periodCount, sendCount = 0;
@@ -174,11 +174,11 @@ unsigned long accumulator(void) {
 			if (pollCounter == pollTimeout) {
 				soundFeedback(reject);
 				Serial.print(messages.cancel);
-				endTime = (millis() - startTime)/1000;
+				sendCount = (millis() - startTime)/1000;
 				while(1) {
-					Serial.println(endTime);
+					Serial.println(sendCount);
 				}
-				return endTime;
+				return sendCount;
 			}
 		}
 		else {
