99767779e943c3a450e6789789fe394fbc09ffad
Brian Nguyen <briannguyen992@gmail.com>
2016-07-04 10:53:10 -0700

    Found that when cutter is off, noise can trigger a log to send

--- unit tests/pwm_detector_test/pwm_detector_test.ino
+++ unit tests/pwm_detector_test/pwm_detector_test.ino
@@ -16,7 +16,7 @@ const bool debug = true;
 const unsigned long minCount = 5;
 const unsigned long lowerBound = 1;
 const unsigned long upperBound = 7;
-const unsigned long pollInterval = 1000;
+const unsigned long pollInterval = 2000;
 const unsigned long sendInterval = 20000;
 
 unsigned long startTime, endTime = 0;
