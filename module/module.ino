cceb41a61a33049182aa7bc3b682ac5a142e3ced
Brian Nguyen <briannguyen992@gmail.com>
2016-08-08 12:02:29 -0700

    Testing LED blink when card not detected during job 3

--- module/module.ino
+++ module/module.ino
@@ -163,7 +163,7 @@ unsigned long accumulator(unsigned char serialNumber[], unsigned long elapsedTim
 			sendCommand(CMD_GET_SNR, blockID, machineID, keyA, NULL);
 			// if card is missing, increment a counter and blink LED?? too slow
 			if (!getResponse(serialNumber)) {
-				//digitalWrite(ledPin, (digitalRead(ledPin) == HIGH ? LOW : HIGH));
+				digitalWrite(ledPin, (digitalRead(ledPin) == HIGH ? LOW : HIGH));
 				pollCounter += 1;
 				// if the counter reaches a specified timeout, return
 				if (pollCounter == pollTimeout) {
@@ -253,7 +253,7 @@ unsigned long accumulator(unsigned char serialNumber[], unsigned long elapsedTim
 			//delay(pollInterval);
 			lastPollTime = millis();
 		}
-		
+
 		unsigned long lastBlink = 0;
 		if (pollCounter > 0) {
 			if ( (millis() - lastBlink) > 500 ) {
