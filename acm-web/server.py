4f8228cae2ac12e8db46876073cf4da58bcac734
Brian Nguyen <briannguyen992@gmail.com>
2016-09-29 20:18:47 -0700

    Testing restful api on front desk mac

--- acm-web/server.py
+++ acm-web/server.py
@@ -36,7 +36,7 @@ def serialTest():
 class HelloWorld(Resource):
 	def get(self):
 		return {'hello': 'world'}
-api.add_resource(HelloWorld, '/hello')
+api.add_resource(HelloWorld, '/helloWorld')
 
 if (__name__ == "__main__"):
     app.run(host='0.0.0.0')
