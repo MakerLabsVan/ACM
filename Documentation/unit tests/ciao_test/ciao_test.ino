e656df8ef354f0f495c74512f8bce1b7288c0301
Brian Nguyen <briannguyen992@gmail.com>
2016-09-29 19:40:29 -0700

    Testing Arduino WiFi with web app

--- Documentation/unit tests/ciao_test/ciao_test.ino
+++ Documentation/unit tests/ciao_test/ciao_test.ino
@@ -1,19 +1,33 @@
 #include <Ciao.h>
 
 #define CONNECTOR "rest"
-#define HOSTNAME "Front-Desk.local"
+#define HOSTNAME "Front-Desk.local:5000"
 #define ADDRESS "192.168.0.58"
 #define URI "/serialTest"
+
+#define ADDRESS_TEST "api.thingspeak.com"
+#define URI_TEST "/update?api_key=HWX6PI5X7NE99VPA&field1=500"
 
 void setup() {
     Ciao.begin();
     Serial.begin(57600);
+    Serial.println(F("Initializing..."));
 }
 
 void loop() {
-    Ciaodata data = Ciao.write(CONNECTOR, ADDRESS, URI);
 
+    Serial.println(F("Attempting to connect..."));
+    CiaoData data = Ciao.write(CONNECTOR, HOSTNAME, URI);
+    //CiaoData data = Ciao.write(CONNECTOR, ADDRESS_TEST, URI_TEST);
 
-
-    delay(10000);
+    if (!data.isEmpty()) {
+        Serial.println(data.get(1));
+        Serial.println(data.get(2));
+    }
+    else {
+        Serial.println(F("Write Error"));
+    }
+    
+    Serial.println();
+    delay(20000);
 }
