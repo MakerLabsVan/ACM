909cb2982d42e47fb8a26f7c5bc9cdaa8d16a832
Brian Nguyen <briannguyen992@gmail.com>
2016-10-06 17:18:36 -0700

    Currently working, but need to integrate with angular

--- acm-web/static/js/views.js
+++ acm-web/static/js/views.js
@@ -15,3 +15,13 @@ $(function() {
     });
 
 });
+
+$(document).ready(function() {
+    var socket = io.connect('http://localhost:5000');
+    console.log("Socket connected.");
+
+    socket.on('scan', function(msg) {
+        console.log("hi from arduino: " + msg.data);
+    });
+
+});
